5245e4d764ef7b94416688be543edfe0c5052717
Ahmed Mahfouz <ahmed98mahfouz@gmail.com>
2025-09-27 21:55:55 +0300

    edit map

--- resources/views/front/pages/contact.blade.php
+++ resources/views/front/pages/contact.blade.php
@@ -393,190 +393,147 @@
             };
 
             // Dark/Black map style for Google Maps JavaScript API
-            const mapStyles = [
-                {
+            const mapStyles = [{
                     "elementType": "geometry",
-                    "stylers": [
-                        {
-                            "color": "#212121"
-                        }
-                    ]
+                    "stylers": [{
+                        "color": "#212121"
+                    }]
                 },
                 {
                     "elementType": "labels.icon",
-                    "stylers": [
-                        {
-                            "visibility": "off"
-                        }
-                    ]
+                    "stylers": [{
+                        "visibility": "off"
+                    }]
                 },
                 {
                     "elementType": "labels.text.fill",
-                    "stylers": [
-                        {
-                            "color": "#757575"
-                        }
-                    ]
+                    "stylers": [{
+                        "color": "#757575"
+                    }]
                 },
                 {
                     "elementType": "labels.text.stroke",
-                    "stylers": [
-                        {
-                            "color": "#212121"
-                        }
-                    ]
+                    "stylers": [{
+                        "color": "#212121"
+                    }]
                 },
                 {
                     "featureType": "administrative",
                     "elementType": "geometry",
-                    "stylers": [
-                        {
-                            "color": "#757575"
-                        }
-                    ]
+                    "stylers": [{
+                        "color": "#757575"
+                    }]
                 },
                 {
                     "featureType": "administrative.country",
                     "elementType": "labels.text.fill",
-                    "stylers": [
-                        {
-                            "color": "#9e9e9e"
-                        }
-                    ]
+                    "stylers": [{
+                        "color": "#9e9e9e"
+                    }]
                 },
                 {
                     "featureType": "administrative.land_parcel",
-                    "stylers": [
-                        {
-                            "visibility": "off"
-                        }
-                    ]
+                    "stylers": [{
+                        "visibility": "off"
+                    }]
                 },
                 {
                     "featureType": "administrative.locality",
                     "elementType": "labels.text.fill",
-                    "stylers": [
-                        {
-                            "color": "#bdbdbd"
-                        }
-                    ]
+                    "stylers": [{
+                        "color": "#bdbdbd"
+                    }]
                 },
                 {
                     "featureType": "poi",
                     "elementType": "labels.text.fill",
-                    "stylers": [
-                        {
-                            "color": "#757575"
-                        }
-                    ]
+                    "stylers": [{
+                        "color": "#757575"
+                    }]
                 },
                 {
                     "featureType": "poi.park",
                     "elementType": "geometry",
-                    "stylers": [
-                        {
-                            "color": "#181818"
-                        }
-                    ]
+                    "stylers": [{
+                        "color": "#181818"
+                    }]
                 },
                 {
                     "featureType": "poi.park",
                     "elementType": "labels.text.fill",
-                    "stylers": [
-                        {
-                            "color": "#616161"
-                        }
-                    ]
+                    "stylers": [{
+                        "color": "#616161"
+                    }]
                 },
                 {
                     "featureType": "poi.park",
                     "elementType": "labels.text.stroke",
-                    "stylers": [
-                        {
-                            "color": "#1b1b1b"
-                        }
-                    ]
+                    "stylers": [{
+                        "color": "#1b1b1b"
+                    }]
                 },
                 {
                     "featureType": "road",
                     "elementType": "geometry.fill",
-                    "stylers": [
-                        {
-                            "color": "#2c2c2c"
-                        }
-                    ]
+                    "stylers": [{
+                        "color": "#2c2c2c"
+                    }]
                 },
                 {
                     "featureType": "road",
                     "elementType": "labels.text.fill",
-                    "stylers": [
-                        {
-                            "color": "#8a8a8a"
-                        }
-                    ]
+                    "stylers": [{
+                        "color": "#8a8a8a"
+                    }]
                 },
                 {
                     "featureType": "road.arterial",
                     "elementType": "geometry",
-                    "stylers": [
-                        {
-                            "color": "#373737"
-                        }
-                    ]
+                    "stylers": [{
+                        "color": "#373737"
+                    }]
                 },
                 {
                     "featureType": "road.highway",
                     "elementType": "geometry",
-                    "stylers": [
-                        {
-                            "color": "#3c3c3c"
-                        }
-                    ]
+                    "stylers": [{
+                        "color": "#3c3c3c"
+                    }]
                 },
                 {
                     "featureType": "road.highway.controlled_access",
                     "elementType": "geometry",
-                    "stylers": [
-                        {
-                            "color": "#4e4e4e"
-                        }
-                    ]
+                    "stylers": [{
+                        "color": "#4e4e4e"
+                    }]
                 },
                 {
                     "featureType": "road.local",
                     "elementType": "labels.text.fill",
-                    "stylers": [
-                        {
-                            "color": "#616161"
-                        }
-                    ]
+                    "stylers": [{
+                        "color": "#616161"
+                    }]
                 },
                 {
                     "featureType": "transit",
                     "elementType": "labels.text.fill",
-                    "stylers": [
-                        {
-                            "color": "#757575"
-                        }
-                    ]
+                    "stylers": [{
+                        "color": "#757575"
+                    }]
                 },
                 {
                     "featureType": "water",
                     "elementType": "geometry",
-                    "stylers": [
-                        {
-                            "color": "#000000"
-                        }
-                    ]
+                    "stylers": [{
+                        "color": "#000000"
+                    }]
                 },
                 {
                     "featureType": "water",
                     "elementType": "labels.text.fill",
-                    "stylers": [
-                        {
-                            "color": "#3d3d3d"
-                        }
-                    ]
+                    "stylers": [{
+                        "color": "#3d3d3d"
+                    }]
                 }
             ];
 
@@ -637,7 +594,7 @@
                                style="
                                    display: inline-block;
                                    padding: 8px 16px;
-                                   background: #4285f4;
+                                   background: #030cfc;
                                    color: white;
                                    text-decoration: none;
                                    border-radius: 4px;
